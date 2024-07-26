31ef6a6043a8239d04e06119e1701608edebd523
jpaulopereira <joaopaulo.ae@gmail.com>
2024-07-26 11:25:10 -0300

    initial

---
+++ src/main/java/br/com/jotape/relatorio/RelatorioApplication.java
@@ -0,0 +1,12 @@
+package br.com.jotape.relatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelatorioApplication.class, args);
+    }
+}
